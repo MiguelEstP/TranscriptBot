2e0cdade4705e33816710c671340dd761e8f80c7
Miguel Estivalet Pinto <miguelestivaletpinto@gmail.com>
2024-11-02 19:01:51 -0300

    recomeçando.py
    startando do 0 pra ver se eu consigo fazer funcionar (agr pelo menos baixa o áudio do vídeo)

--- main.py
+++ main.py
@@ -1,43 +1,18 @@
-# Analisador de Vídeos do YT (vídeo -> script)
-# imports gerais
-import pytubefix
-import ffmpeg
-import openai
+#Bot que pela URL do YT consegue transcrever para um arquivo de testo markdown
+#Baixar o audio do vídeo do YT
+import yt_dlp
 
-# OpenAIkey
-openai.api_key = "Coloque sua API Key aq"
+link = input("Link do vídeo: ")
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': 'D:\Miguel\Downloads\Vídeos/%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
 
-# Importar a URL dele (baixar o áudio)
-import sys
-url = sys.argv[1]
-filename = "audio.wav"
-yt = pytubefix.YouTube(url)
-stream = yt.streams.filter(only_audio=True).first().url
-ffmpeg.input(stream).output(filename,
-                            format='wav',
-                            loglevel="error")
-
-# Transcrever o audio para texto
-with open(filename, "rb") as audio_file:
-    transcript = openai.audio.transcriptions.create(
-        model="whisper-1",
-        file=audio_file
-    ).text
-
-# Gerar um script
-completion = openai.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system",
-         "content": """
-         Você é um assistem que resume videos detalhando eles.
-         Responda com formatação Markdown.
-         """},
-
-         {"role": "user",
-          "content": f"Descreva o seguinde vídeo: {transcript}"}
-    ])
-
-# Salvar o resumo em um arquivo Markdown
-with open("resumo.md", "w+") as md_file:
-    md_file.write(completion.choices[0].message.content)
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([link])
+print("O áudio foi baixado com sucesso.")
